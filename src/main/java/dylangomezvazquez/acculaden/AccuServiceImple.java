9db31e0b6a2080d0ca32986df7de54ae93a0963c
dylangomez <dylangomezvazquez@hotmail.com>
2017-03-11 19:36:50 +0100

    Added soapmessages for assignment.

--- src/main/java/dylangomezvazquez/acculaden/AccuServiceImple.java
+++ src/main/java/dylangomezvazquez/acculaden/AccuServiceImple.java
@@ -16,22 +16,34 @@ public class AccuServiceImple implements WSInterface {
 
         ObjectFactory factory = new ObjectFactory();
         AccuLadenResponse response = factory.createAccuLadenResponse();
+        int vermogen = parameters.getVermogen();
+        double procent = parameters.getProcent();
+        int voltage = parameters.getDuur();
+        int uur = parameters.getVermogen() / 10;
+//        wilde na veel moeite niet naar behoren werken
+//        double remaining = uur / (procent/100);
 
         try{
 
-            Integer vermogen = parameters.getVermogen() * parameters.getVermogen();
-            String procent = "De accu heeft heeft een vermogen van "+ parameters.getVermogen() + ", de accu heeft momenteel nog "+ parameters.getProcent()
-                    + " procent over opladen bij het huidige tankstation duurt: " + parameters.getDuur() + " minuten";
-            System.out.println(procent);
+            if ( vermogen > 0 && procent >= 0 && procent <= 100 && voltage > 0 && vermogen ==(int)vermogen && procent ==(double)procent
+                    && voltage ==(int)voltage) {
+//                if (uur == remaining){
+//                    remaining = remaining - uur;
+//                }
+                String successtring = "De accu heeft heeft een vermogen van " + vermogen + " Ampére, \n de accu heeft momenteel nog " + procent
+                        + " procent over. De batterij heeft een voltage van " + voltage + " volt \n" +
+                        "opladen van 0 tot 100% duurt: " + uur + " uur";
 
-            response.setResult(procent.toString());
-
+                response.setResult(successtring.toString());
+            }else{
+                throw new RuntimeException();
+            }
         }catch (RuntimeException e){
 
             dylangomezvazquez.acculadenerror.ObjectFactory errorFactory = new dylangomezvazquez.acculadenerror.ObjectFactory();
             dylangomezvazquez.acculadenerror.ApplicatieFout x = errorFactory.createApplicatieFout();
             x.setErrorCode(404);
-            x.setMessage("Value cannot be found");
+            x.setMessage("Values not accepted ");
 
             ApplicatieFout fout = new ApplicatieFout("Meegegeven accu gegevens zijn niet goed", x);
             throw fout;
